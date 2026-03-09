bf71c4d7c6abc49d159d21f89f2ee27e4fb19945
TOMUSANGE-MARVIN <marvintomusange@gmail.com>
2026-03-09 16:09:20 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/OurTribe.tsx
+++ src/components/OurTribe.tsx
@@ -5,32 +5,32 @@ import ScrollAnimate from './ScrollAnimate';
 const teamMembers = [
   {
     name: 'Pesh',
-    image: '/images/team/pesh.jpg',
+    image: '/images/team/Pesh.png',
     bio: 'Pesh comes to Dawn Elements with experience that spans from tax advisory to climate-smart agribusiness and value addition. For over 10 years, she worked in the climate-smart agribusiness production value chain, from supporting farming communities, to managing manufacturing processes. Eventually, negotiating cutting-edge deals to move products across Africa, Europe and Asia. She is a blazing free spirit, and a karaoke regular.',
   },
   {
     name: 'Eshban',
-    image: '/images/team/eshban.jpg',
+    image: '/images/team/Eshban.png',
     bio: 'Eshban has built experience applying deep systems thinking to an evolving development context. His early career started in the private sector and later evolved to international development and philanthropy. He has over 13 years experience advising impact investment and development organizations as well as governments on creative solutions to complex challenges. He has published on agriculture, macro-financial policy, feminism, energy, and governance. He needs no convincing for plans that involve spending time at the beach.',
   },
   {
     name: 'Pross',
-    image: '/images/team/pross.jpg',
+    image: '/images/team/Pross.png',
     bio: 'Pross is our "resident" tax expert, with over a decade of experience, advising on tax solutions for diverse clients, from multinationals to local firms, on tax compliance, optimization, and risk management. She is a magician at designing tax-efficient structures, managing risks, and navigating international taxation complexities. Her thought-leadership extends to publications and high-level forums advising on compliance mechanisms. Her goal at Dawn Elements is to leverage her expertise for impactful social investments and advancing human progress. Pross is "a nomad at heart" and is always ready to indulge in a good travel experience.',
   },
   {
     name: 'Eva',
-    image: '/images/team/eva.jpg',
+    image: '/images/team/Eva.png',
     bio: 'With over a decade of experience in social impact, she is a driving force dedicated to addressing critical societal challenges through impact work in social development and community-led actions. Her expertise extends to strategic venture philanthropy, insights on sustainable development and designing system solutions. At Dawn Elements, she advises making impactful social investments and shaping narratives for human progress. Eva will "leave it all behind," if she was ever offered an opportunity to play in the soccer world cup.',
   },
   {
     name: 'Twasiima',
-    image: '/images/team/twasiima.jpg',
+    image: '/images/team/Twasiima.png',
     bio: 'Twasiima Bigirwa is a writer and artist. For more than 10 years, Twasiima worked as a lawyer, and later in philanthropy and impact investment. She has an incredible ability to cut through the noise and to see or say things for what they are. Today she spends most of her time in liminal spaces, and currently lives in the UAE where she is in between writing that and editing this for her fourth book.',
   },
   {
     name: 'Stuart',
-    image: '/images/team/stuart.jpg',
+    image: '/images/team/Stuart.png',
     bio: 'Stuart AKA "Design Carpenter" leads our design thinking work. He has worked extensively across both for-profit and non-profit organisations supporting them to elevate narratives that work. He is our resident design thinker, who moves seamlessly across abstract and practical matters, and is also a farmer. A true problem solver in all the ways.',
   },
 ];
